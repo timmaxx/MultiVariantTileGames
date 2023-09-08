fd89b1ddcf365de0ea1e667b2eea6deb9f191be7
“Maksim <m.n.timchenko@gmail.com>
2023-09-08 15:14:59 +0300

    refactor GameStackPane

--- TileGameEngineJFX/src/main/java/timmax/tilegameenginejfx/GameStackPane.java
+++ TileGameEngineJFX/src/main/java/timmax/tilegameenginejfx/GameStackPane.java
@@ -8,59 +8,69 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class GameStackPane extends StackPane {
-    private final Rectangle rectangle;
-    private final Text generalText;
-    private final Text coordinateText;
+    private static final double COORDINATE_TEXT_FONT_SIZE_COEFFICIENT = 0.15;
+
+    private static final int MAX_GENERAL_TEXT_LENGTH_FOR_GENERAL_TEXT_FONT_SIZE_COEFFICIENT = 4;
+    private static final double GENERAL_TEXT_FONT_SIZE_COEFFICIENT = 0.4;
 
 
     public GameStackPane( int x, int y, int cellSize, boolean showGrid, boolean showCoordinates, int PADDING_SIDE, int PADDING_TOP) {
         super( new Rectangle( ), new Text( ), new Text( ));
 
-        rectangle = ( ( Rectangle)getChildren( ).get( 0));
-        generalText = ( ( Text)getChildren( ).get( 1));
-        coordinateText = ( ( Text)getChildren( ).get( 2));
-
         if ( showGrid) {
-            rectangle.setWidth( cellSize - 1);
-            rectangle.setHeight( cellSize - 1);
-            rectangle.setStroke( Color.BLACK);
+            getRectangle( ).setWidth( cellSize - 1);
+            getRectangle( ).setHeight( cellSize - 1);
+            getRectangle( ).setStroke( Color.BLACK);
         }
         if ( showCoordinates) {
-            coordinateText.setFont( Font.font( cellSize * 0.15));
-            StackPane.setAlignment( coordinateText, Pos.TOP_LEFT);
-            coordinateText.setText( x + " - " + y);
+            getCoordinateText( ).setFont( Font.font( cellSize * COORDINATE_TEXT_FONT_SIZE_COEFFICIENT));
+            StackPane.setAlignment( getCoordinateText( ), Pos.TOP_LEFT);
+            getCoordinateText( ).setText( x + " - " + y);
         }
-        rectangle.setWidth( cellSize);
-        rectangle.setHeight( cellSize);
+        getRectangle( ).setWidth( cellSize);
+        getRectangle( ).setHeight( cellSize);
         setLayoutX( x * cellSize + PADDING_SIDE);
         setLayoutY( y * cellSize + PADDING_TOP);
     }
 
+    private Rectangle getRectangle( ) {
+        return ( ( Rectangle)getChildren( ).get( 0));
+    }
+
+    private Text getGeneralText( ) {
+        return ( ( Text)getChildren( ).get( 1));
+    }
+
+    private Text getCoordinateText( ) {
+        return ( ( Text)getChildren( ).get( 2));
+    }
+
     public void setCellValue( String textValue, int cellSize) {
-        if ( generalText.getText( ).equals( textValue)) {
+        if ( getGeneralText( ).getText( ).equals( textValue)) {
             return;
         }
-        if ( textValue.length( ) < 5) {
-            double fontSize = cellSize * 0.4;
-            generalText.setFont( Font.font( fontSize));
+        if ( textValue.length( ) <= MAX_GENERAL_TEXT_LENGTH_FOR_GENERAL_TEXT_FONT_SIZE_COEFFICIENT) {
+            double fontSize = cellSize * GENERAL_TEXT_FONT_SIZE_COEFFICIENT;
+            getGeneralText( ).setFont( Font.font( fontSize));
         } else {
             int fontSize = cellSize / textValue.length( );
-            generalText.setFont( Font.font( fontSize));
+            getGeneralText( ).setFont( Font.font( fontSize));
         }
-        generalText.setText( textValue);
+        getGeneralText( ).setText( textValue);
     }
 
     public void setCellColor( Color cellColor) {
-        if ( cellColor != null && cellColor != Color.TRANSPARENT) {
-            if ( !cellColor.equals( rectangle.getFill( ))) {
-                rectangle.setFill( cellColor);
-            }
+        if (    cellColor != null &&
+                cellColor != Color.TRANSPARENT &&
+                !cellColor.equals( getRectangle( ).getFill( ))
+        ) {
+            getRectangle( ).setFill( cellColor);
         }
     }
 
     public void setCellTextColor( Color textColor) {
-        if ( !textColor.equals( generalText.getFill( ))) {
-            generalText.setFill( textColor);
+        if ( !textColor.equals( getGeneralText( ).getFill( ))) {
+            getGeneralText( ).setFill( textColor);
         }
     }
 
