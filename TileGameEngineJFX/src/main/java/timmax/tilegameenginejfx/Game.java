eee7ab1d5c29e895437bee6cec1e9ba45e40c8f7
“Maksim <m.n.timchenko@gmail.com>
2023-09-25 22:32:02 +0300

    refactor TileGameEngineJfx package (part 4.1) for separation of SceneController and StackPaneController

--- TileGameEngineJFX/src/main/java/timmax/tilegameenginejfx/Game.java
+++ TileGameEngineJFX/src/main/java/timmax/tilegameenginejfx/Game.java
@@ -1,11 +1,13 @@
 package timmax.tilegameenginejfx;
 
+import java.util.List;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 import timmax.basetilemodel.BaseModel;
 
-public abstract class Game extends Application implements GameScreen {
+public abstract class Game extends Application {
     public final static int APP_WIDTH = 800; // 1600;
     public final static int APP_HEIGHT = 600; // 1200
 
@@ -22,6 +24,9 @@ public abstract class Game extends Application implements GameScreen {
         ViewJfx viewMainFieldJfx = initViewOfMainField( baseModel, gameStackPaneController);
         root.getChildren( ).add( viewMainFieldJfx); // viewMainFieldJfx - Наследник ViewJfx, а ViewJfx должен быть наследником Node
 
+        List< Node> nodeList = initNodeList( baseModel);
+        root.getChildren( ).addAll( nodeList);
+
         GameSceneController gameSceneController = initGameSceneController( baseModel);
         GameScene scene = new GameScene( root, gameSceneController);
 
@@ -33,4 +38,16 @@ public abstract class Game extends Application implements GameScreen {
 
         primaryStage.show( );
     }
+
+    abstract protected List< Node> initNodeList( BaseModel baseModel);
+
+    abstract protected BaseModel initModel( );
+
+    abstract protected ViewJfx initViewOfMainField( BaseModel baseModel, GameStackPaneController gameStackPaneController);
+
+    abstract protected String initAppTitle( );
+
+    abstract protected GameSceneController initGameSceneController( BaseModel baseModel);
+
+    abstract protected GameStackPaneController initGameStackPaneController( BaseModel basemodel);
 }
