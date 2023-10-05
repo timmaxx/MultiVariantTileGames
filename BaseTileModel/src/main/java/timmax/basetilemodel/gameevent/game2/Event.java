2c1e83a0fb3a0a928f3bc8fe336a58e336735fcf
“Maksim <m.n.timchenko@gmail.com>
2023-10-05 22:53:11 +0300

    delete the alternative branch of message realization in the BaseModel

--- BaseTileModel/src/main/java/timmax/basetilemodel/gameevent/game2/Event.java
+++
@@ -1,6 +0,0 @@
-package timmax.basetilemodel.gameevent.game2;
-
-public class Event {
-    // private Game2TypeOfEvent game2TypeOfEvent;
-
-}
--- BaseTileModel/src/main/java/timmax/basetilemodel/gameevent/game2/TypeOfEvent.java
+++
@@ -1,8 +0,0 @@
-package timmax.basetilemodel.gameevent.game2;
-
-// 4
-public enum TypeOfEvent {
-    NEW_GAME,
-    GAME_OVER,
-    ONE_TILE
-}
