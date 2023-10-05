d5bb1b2ec38e992e34f46a6e39a14f7134d76f65
“Maksim <m.n.timchenko@gmail.com>
2023-10-05 23:12:29 +0300

    refactor GameEventSokoban... in SokobanModel for unify header constants in SokobanControlsAndViewsJfx

--- SokobanModel/src/main/java/timmax/sokoban/model/gameevent/GameEventSokobanVariableParamsCountOfSteps.java
+++ SokobanModel/src/main/java/timmax/sokoban/model/gameevent/GameEventSokobanVariableParamsCountOfSteps.java
@@ -3,7 +3,11 @@ package timmax.sokoban.model.gameevent;
 import timmax.basetilemodel.gameevent.GameEvent;
 
 public class GameEventSokobanVariableParamsCountOfSteps extends GameEvent {
+    // ToDo: Разобраться и удалить ведущий '\n'
+    public final static String COMMON_LABEL_OF_VARIABLE_PARAMS_COUNT_OF_STEPS = "\nVariable settings - Count of steps = ";
+
     private final int countOfSteps;
+
 
     public GameEventSokobanVariableParamsCountOfSteps( int countOfSteps) {
         this.countOfSteps = countOfSteps;
