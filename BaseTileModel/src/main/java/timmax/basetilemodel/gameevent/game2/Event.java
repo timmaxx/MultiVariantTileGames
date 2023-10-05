1cbfee82f5b8cde8885e0864de3637e112f4d2f3
“Maksim <m.n.timchenko@gmail.com>
2023-10-05 22:51:07 +0300

    An attempt to implement an alternative messaging mechanism in the BaseModel. To be deleted

---
+++ BaseTileModel/src/main/java/timmax/basetilemodel/gameevent/game2/Event.java
@@ -0,0 +1,6 @@
+package timmax.basetilemodel.gameevent.game2;
+
+public class Event {
+    // private Game2TypeOfEvent game2TypeOfEvent;
+
+}
---
+++ BaseTileModel/src/main/java/timmax/basetilemodel/gameevent/game2/TypeOfEvent.java
@@ -0,0 +1,8 @@
+package timmax.basetilemodel.gameevent.game2;
+
+// 4
+public enum TypeOfEvent {
+    NEW_GAME,
+    GAME_OVER,
+    ONE_TILE
+}
