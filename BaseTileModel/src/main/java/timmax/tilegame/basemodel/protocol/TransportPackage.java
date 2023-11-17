3e53b5e9cba591e19b12d12cac44344fe9a13041
“Maksim <m.n.timchenko@gmail.com>
2023-11-17 09:04:06 +0300

    refactor in TransportPackage string constants

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/TransportPackage.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/TransportPackage.java
@@ -11,12 +11,16 @@ import timmax.tilegame.basemodel.protocol.exception.MultiGameProtocolException;
 import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
 
 public abstract class TransportPackage {
-    private final static String ERROR_MESSAGE_PARAMETERS_LIST_IS_EMPTY_BUT_IT_IS_WRONG =
-            "Transport package is '%s'. Type of transport package is '%s'. There are not any parameters, but they must be %d.";
-    private final static String ERROR_MESSAGE_COUNT_OF_PARAMETERS_IN_SPECIFICATION_IS_NOT_EQUAL_COUNT_OF_PARAMETERS_IN_THIS_PACKAGE =
-            "\nTransport package is '%s'. \nType of transport package is '%s'. \nCount of parameters in specification of protocol is %d and it is not equal count of parameters in package %d.";
-    private final static String ERROR_MESSAGE_TYPE_OF_PARAMETER_IN_SPECIFICATION_IS_NOT_EQUAL_TYPE_OF_PARAMETERS_IN_THIS_PACKAGE =
-            "\nTransport package is '%s'. \nType of transport package is '%s'. \nType of parameter '%s' must be type of '%s'. But received value = '%s' is type of '%s'.\n";
+    private final static String TRANSPORT_PACKAGE_AND_ITS_TYPE =
+            "\nTransport package is '%s'. \nType of transport package is '%s'.";
+    private final static String KEY_IN_MAP_OF_PARAM_VALUE_DOES_NOT_CONTAINS_KEY_IN_MAP_OF_PARAM_CLASS =
+            "\nmapOfParamName_Value does not contains key = '%s'\nmapOfParamName_Value = '%s'\nmapOfParamName_Class = '%s'";
+    private final static String PARAMETERS_LIST_IS_EMPTY_BUT_IT_IS_WRONG =
+            "\nThere are not any parameters, but they must be %d.";
+    private final static String COUNT_OF_PARAMETERS_IN_SPECIFICATION_IS_NOT_EQUAL_COUNT_OF_PARAMETERS_IN_THIS_PACKAGE =
+            "\nCount of parameters in specification of protocol is %d and it is not equal count of parameters in package %d.";
+    private final static String TYPE_OF_PARAMETER_IN_SPECIFICATION_IS_NOT_EQUAL_TYPE_OF_PARAMETERS_IN_THIS_PACKAGE =
+            "\nType of parameter '%s' must be type of '%s'. But received value = '%s' is type of '%s'.\n";
 
     private final MapOfStructOfTransportPackage mapOfStructOfTransportPackage;
     private final TypeOfTransportPackage typeOfTransportPackage;
@@ -28,7 +32,9 @@ public abstract class TransportPackage {
         Map<String, Class<?>> mapOfParamName_Class = mapOfStructOfTransportPackage.getMapParamName_ClassByTypeOfTransportPackage(typeOfTransportPackage);
 
         if (!mapOfParamName_Class.isEmpty()) {
-            throw new RuntimeException(String.format(ERROR_MESSAGE_PARAMETERS_LIST_IS_EMPTY_BUT_IT_IS_WRONG, typeOfTransportPackage.getClass(), typeOfTransportPackage, mapOfParamName_Class.size()));
+            String head = String.format(TRANSPORT_PACKAGE_AND_ITS_TYPE, typeOfTransportPackage.getClass(), typeOfTransportPackage);
+            String part2 = String.format(PARAMETERS_LIST_IS_EMPTY_BUT_IT_IS_WRONG, mapOfParamName_Class.size());
+            throw new RuntimeException(head + part2);
         }
 
         this.typeOfTransportPackage = typeOfTransportPackage;
@@ -45,8 +51,10 @@ public abstract class TransportPackage {
         StringBuilder stringBuilder = new StringBuilder();
 
         if (mapOfParamName_Class.size() != mapOfParamName_Value.size()) {
-            stringBuilder.append(String.format(ERROR_MESSAGE_COUNT_OF_PARAMETERS_IN_SPECIFICATION_IS_NOT_EQUAL_COUNT_OF_PARAMETERS_IN_THIS_PACKAGE,
-                    typeOfTransportPackage.getClass(), typeOfTransportPackage, mapOfParamName_Class.size(), mapOfParamName_Value.size()));
+            stringBuilder.append(String.format(TRANSPORT_PACKAGE_AND_ITS_TYPE,
+                    typeOfTransportPackage.getClass(), typeOfTransportPackage));
+            stringBuilder.append(String.format(COUNT_OF_PARAMETERS_IN_SPECIFICATION_IS_NOT_EQUAL_COUNT_OF_PARAMETERS_IN_THIS_PACKAGE,
+                    mapOfParamName_Class.size(), mapOfParamName_Value.size()));
         } else {
 /*
             System.out.println("mapOfParamName_Class = " + mapOfParamName_Class);
@@ -58,9 +66,8 @@ public abstract class TransportPackage {
 //              System.out.println("paramName = " + paramName);
 
                 if (!mapOfParamName_Value.containsKey(paramName)) {
-                    stringBuilder.append("\nmapOfParamName_Value does not contains key = '" + paramName + "'");
-                    stringBuilder.append("\nmapOfParamName_Value = " + mapOfParamName_Value);
-                    stringBuilder.append("\nmapOfParamName_Class = " + mapOfParamName_Class);
+                    stringBuilder.append(String.format(KEY_IN_MAP_OF_PARAM_VALUE_DOES_NOT_CONTAINS_KEY_IN_MAP_OF_PARAM_CLASS,
+                            paramName, mapOfParamName_Value, mapOfParamName_Class));
                     break;
                 }
 
@@ -71,8 +78,10 @@ public abstract class TransportPackage {
 */
                 if ((value.getClass() != String.class || !clazz.isEnum())
                         && (!Classes.isInstanceOf(value, clazz))) {
-                    stringBuilder.append(String.format(ERROR_MESSAGE_TYPE_OF_PARAMETER_IN_SPECIFICATION_IS_NOT_EQUAL_TYPE_OF_PARAMETERS_IN_THIS_PACKAGE,
-                            typeOfTransportPackage.getClass(), typeOfTransportPackage, paramName, clazz, value, value.getClass()));
+                    stringBuilder.append(String.format(TRANSPORT_PACKAGE_AND_ITS_TYPE,
+                            typeOfTransportPackage.getClass(), typeOfTransportPackage));
+                    stringBuilder.append(String.format(TYPE_OF_PARAMETER_IN_SPECIFICATION_IS_NOT_EQUAL_TYPE_OF_PARAMETERS_IN_THIS_PACKAGE,
+                            paramName, clazz, value, value.getClass()));
                 }
             }
         }
