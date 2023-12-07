cff1390c2c9d222bf729bfe56eb0eccf80efb55a
“Maksim <m.n.timchenko@gmail.com>
2023-12-07 18:04:56 +0300

    implement Externalizable for descendant of GameEvent classes in BaseTileModel module

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/gameevent/GameEventOneTile.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/gameevent/GameEventOneTile.java
@@ -1,9 +1,20 @@
 package timmax.tilegame.basemodel.gameevent;
 
-public abstract class GameEventOneTile extends GameEvent {
-    private final int x;
-    private final int y;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
+public abstract class GameEventOneTile extends GameEvent {
+    // final (в этом классе и в любом, который реализует Externalizable) пришлось убрать из-за readExternal.
+    // Было-бы лучше конечно final оставить!
+    // Да и конструктор без параметров - тоже для Externalizable, и лучше-бы без такого конструктора обойтись.
+    // А так можно было-бы свой интерфейс сделать с конструктором, у которого был-бы параметром массив объектов!
+    private /*final*/ int x;
+    private /*final*/ int y;
+
+
+    public GameEventOneTile() {
+    }
 
     public GameEventOneTile(int x, int y) {
         this.x = x;
@@ -16,5 +27,17 @@ public abstract class GameEventOneTile extends GameEvent {
 
     public int getY() {
         return y;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeInt(x);
+        out.writeInt(y);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        x = in.readInt();
+        y = in.readInt();
     }
 }
