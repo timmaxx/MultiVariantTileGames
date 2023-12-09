85128f267049fb97530bdabcb31883189cfe734e
“Maksim <m.n.timchenko@gmail.com>
2023-12-10 02:34:25 +0300

    separate incoming message from its handling and sending outcoming message in WebSocketServer package

---
+++ WebSocketServer/src/main/java/timmax/tilegame/websocket/server/IncomingMessageHandler.java
@@ -0,0 +1,183 @@
+package timmax.tilegame.websocket.server;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.java_websocket.WebSocket;
+
+import timmax.tilegame.basemodel.credential.Credentials;
+import timmax.tilegame.basemodel.protocol.TransportPackageOfClient;
+import timmax.tilegame.basemodel.protocol.TransportPackageOfServer;
+import timmax.tilegame.basemodel.protocol.TypeOfTransportPackage;
+import timmax.tilegame.basemodel.protocol.server.RemoteView;
+
+//import timmax.tilegame.game.minesweeper.model.MinesweeperModel;
+import timmax.tilegame.game.sokoban.model.ModelOfServerOfSokoban;
+import timmax.tilegame.game.sokoban.model.SokobanModel;
+
+import static java.util.stream.Collectors.toList;
+import static timmax.tilegame.basemodel.protocol.TypeOfTransportPackage.*;
+import static timmax.tilegame.basemodel.protocol.TypeOfTransportPackage.CREATE_NEW_GAME;
+
+public class IncomingMessageHandler {
+    protected MultiGameWebSocketServer multiGameWebSocketServer;
+    protected WebSocket webSocket;
+    protected TransportPackageOfClient transportPackageOfClient;
+
+    public IncomingMessageHandler(MultiGameWebSocketServer multiGameWebSocketServer, WebSocket webSocket, ByteBuffer byteBuffer) {
+        this.multiGameWebSocketServer = multiGameWebSocketServer;
+        this.webSocket = webSocket;
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteBuffer.array());
+        ObjectInput objectInput;
+        try {
+            objectInput = new ObjectInputStream(byteArrayInputStream);
+        } catch (IOException e) {
+            System.out.println("catch (IOException e)");
+            throw new RuntimeException(e);
+        }
+
+        try {
+            transportPackageOfClient = (TransportPackageOfClient) objectInput.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("transportPackageOfClient = " + transportPackageOfClient);
+
+        Thread thread = new Thread(() -> {
+            TypeOfTransportPackage typeOfTransportPackage = transportPackageOfClient.getTypeOfTransportPackage();
+
+            if (typeOfTransportPackage == LOGOUT) {
+                onLogout();
+            } else if (typeOfTransportPackage == LOGIN) {
+                onLogin();
+            } else if (typeOfTransportPackage == FORGET_GAME_TYPE_SET) {
+                onForgetGameTypeSet();
+            } else if (typeOfTransportPackage == GET_GAME_TYPE_SET) {
+                onGetGameTypeSet();
+            } else if (typeOfTransportPackage == FORGET_GAME_TYPE) {
+                onForgetGameType();
+            } else if (typeOfTransportPackage == SELECT_GAME_TYPE) {
+                onSelectGameType();
+            } else if (typeOfTransportPackage == ADD_VIEW) {
+                onAddView();
+            } else if (typeOfTransportPackage == CREATE_NEW_GAME) {
+                onCreateNewGame();
+            } else {
+                System.err.println("Server doesn't know received typeOfTransportPackage.");
+                System.err.println("typeOfTransportPackage = " + typeOfTransportPackage);
+                System.exit(1);
+            }
+            System.out.println("---------- End of public IncomingMessageHandler(MultiGameWebSocketServer multiGameWebSocketServer, WebSocket webSocket, ByteBuffer byteBuffer)");
+        });
+        thread.start();
+    }
+
+    private void onLogout() {
+        System.out.println("onLogout");
+
+        sendLogoutAnswer();
+    }
+
+    private void onLogin() {
+        System.out.println("onLogin");
+
+        String userName = ((String) transportPackageOfClient.get("userName"));
+        String password = ((String) transportPackageOfClient.get("password"));
+
+        System.out.println("userName = " + userName + " | " + "password = *"); // Пароль не выводим:
+
+        if (Credentials.isUserAndPasswordCorrect(userName, password)) {
+            // ToDo: Нужно зафиксировать для этого webSocket имя пользователя (и потом другие параметры авторизации).
+            sendLoginAnswer(userName);
+        } else {
+            sendLogoutAnswer();
+        }
+    }
+
+    private void sendLogoutAnswer() {
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(LOGOUT));
+    }
+
+    private void sendLoginAnswer(String userName) {
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(
+                LOGIN,
+                Map.of("userName", userName)
+        ));
+    }
+
+    private void onForgetGameTypeSet() {
+        System.out.println("onForgetGameTypeSet");
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(FORGET_GAME_TYPE_SET));
+    }
+
+    private void onGetGameTypeSet() {
+        System.out.println("onGetGameTypeSet");
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(
+                GET_GAME_TYPE_SET,
+                Map.of("gameTypeSet",
+                        Stream.of(
+                                // ToDo: Перечень классов вариантов игр следует делать не константами в коде. Варианты:
+                                //       - файл параметров,
+                                //       - классы, хранящиеся по определённому пути.
+                                /*MinesweeperModel.class,*/
+                                SokobanModel.class
+                        ).collect(toList())
+                )
+        ));
+    }
+
+    private void onForgetGameType() {
+        System.out.println("onForgetGameType");
+
+        System.out.println("modelOfServer = " + multiGameWebSocketServer.modelOfServer);
+        multiGameWebSocketServer.modelOfServer = null;
+        System.out.println("modelOfServer = " + multiGameWebSocketServer.modelOfServer);
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(FORGET_GAME_TYPE));
+    }
+
+    private void onSelectGameType() {
+        System.out.println("onSelectGameType");
+
+        // System.out.println("transportPackageOfClient = " + transportPackageOfClient);
+        String model = (String) transportPackageOfClient.get("gameType");
+        // ToDo: Проверить, что model одна из списка возможных моделей, которые были отправлены ранее этому клиенту.
+        //       И если это не так, то отправить клиенту FORGET_GAME_TYPE.
+
+        {   // Создать новую модель
+            System.out.println("modelOfServer = " + multiGameWebSocketServer.modelOfServer);
+            // ToDo: Вместо вызова конкретного конструктора, тут нужно создавать экземпляр того типа, который был выбран клиентом.
+            multiGameWebSocketServer.modelOfServer = new ModelOfServerOfSokoban<>(multiGameWebSocketServer);
+            System.out.println("modelOfServer = " + multiGameWebSocketServer.modelOfServer);
+        }
+
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(
+                SELECT_GAME_TYPE,
+                Map.of("gameType", model)
+        ));
+    }
+
+    private void onAddView() {
+        System.out.println("onAddView");
+
+        String viewId = (String) transportPackageOfClient.get("viewId");
+        // System.out.println("viewId = " + viewId);
+        multiGameWebSocketServer.modelOfServer.addRemoteView(new RemoteView<>(webSocket, viewId));
+
+        multiGameWebSocketServer.send(webSocket, new TransportPackageOfServer(
+                ADD_VIEW,
+                Map.of("viewId", viewId)
+        ));
+    }
+
+    private void onCreateNewGame() {
+        System.out.println("onCreateNewGame");
+
+        multiGameWebSocketServer.modelOfServer.createNewGame();
+    }
+}
--- WebSocketServer/src/main/java/timmax/tilegame/websocket/server/MultiGameWebSocketServer.java
+++ WebSocketServer/src/main/java/timmax/tilegame/websocket/server/MultiGameWebSocketServer.java
@@ -4,28 +4,21 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
-import timmax.tilegame.basemodel.credential.Credentials;
 import timmax.tilegame.basemodel.gameevent.GameEvent;
 import timmax.tilegame.basemodel.protocol.*;
 import timmax.tilegame.basemodel.protocol.server.ModelOfServer;
 import timmax.tilegame.basemodel.protocol.server.RemoteView;
-import timmax.tilegame.game.sokoban.model.ModelOfServerOfSokoban;
 import timmax.tilegame.transport.TransportOfModel;
 
-//import timmax.tilegame.game.minesweeper.model.MinesweeperModel;
-import timmax.tilegame.game.sokoban.model.SokobanModel;
-
-import static java.util.stream.Collectors.toList;
 import static timmax.tilegame.basemodel.protocol.TypeOfTransportPackage.*;
 
 public class MultiGameWebSocketServer extends WebSocketServer implements TransportOfModel<WebSocket> {
-    private ModelOfServer<WebSocket> modelOfServer;
+    ModelOfServer<WebSocket> modelOfServer;
 
     private ObjectOutput objectOutput;
 
@@ -56,10 +49,9 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
 
     public void encodeExternalizable(Externalizable externalizable) throws IOException {
         objectOutput.writeObject(externalizable);
-        System.out.println("----------");
     }
 
-    private void send(WebSocket webSocket, TransportPackageOfServer transportPackageOfServer) {
+    void send(WebSocket webSocket, TransportPackageOfServer transportPackageOfServer) {
         System.out.println("private void send(WebSocket webSocket, TransportPackageOfServer transportPackageOfServer)");
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -68,7 +60,6 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
 
         try {
             encodeExternalizable(transportPackageOfServer);
@@ -86,7 +77,7 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
     @Override
     public void onStart() {
         System.out.println("MultiGameWebSocketServer started on port: " + getPort() + ".");
-        System.err.println("----------");
+        System.err.println("---------- End of onStart");
     }
 
     @Override
@@ -95,7 +86,7 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
         System.out.println(webSocket + ".");
         System.out.println("Connect was closed.");
         System.out.println("Code = " + code + ". Reason = " + reason + ". Remote = " + remote + ".");
-        System.out.println("----------");
+        System.out.println("---------- End of onClose");
     }
 
     @Override
@@ -108,7 +99,7 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
         //       Соответственно, нужна карта, в которой будет храниться:
         //       webSocket, нить, модель игры.
         //       Но, вполне возможно, что это и так уже делается ядром WinSocket...
-        System.out.println("----------");
+        System.out.println("---------- End of onOpen");
     }
 
     @Override
@@ -116,56 +107,16 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
         System.err.println("onError");
         System.err.println(webSocket + ".");
         ex.printStackTrace();
-        System.err.println("----------");
+        System.err.println("---------- End of onError");
     }
 
     @Override
     public void onMessage(WebSocket webSocket, ByteBuffer byteBuffer) {
         System.out.println("onMessage(WebSocket webSocket, ByteBuffer byteBuffer)");
         System.out.println(webSocket);
+        System.out.println("---------- End of onMessage(WebSocket webSocket, ByteBuffer byteBuffer)");
 
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteBuffer.array());
-        ObjectInput objectInput;
-        try {
-            objectInput = new ObjectInputStream(byteArrayInputStream);
-        } catch (IOException e) {
-            System.out.println("catch (IOException e)");
-            throw new RuntimeException(e);
-        }
-        TransportPackageOfClient transportPackageOfClient;
-        try {
-            transportPackageOfClient = (TransportPackageOfClient)objectInput.readObject();
-        } catch (ClassNotFoundException | IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println("transportPackageOfClient = " + transportPackageOfClient);
-
-        TypeOfTransportPackage typeOfTransportPackage = transportPackageOfClient.getTypeOfTransportPackage();
-        if (typeOfTransportPackage == LOGOUT) {
-            // System.out.println("typeOfTransportPackage == LOGOUT");
-            onLogout(webSocket);
-        } else if (typeOfTransportPackage == LOGIN) {
-            // System.out.println("typeOfTransportPackage == LOGIN");
-            onLogin(webSocket, transportPackageOfClient);
-        } else if (typeOfTransportPackage == FORGET_GAME_TYPE_SET) {
-            onForgetGameTypeSet(webSocket);
-        } else if (typeOfTransportPackage == GET_GAME_TYPE_SET) {
-            onGetGameTypeSet(webSocket);
-        } else if (typeOfTransportPackage == FORGET_GAME_TYPE) {
-            onForgetGameType(webSocket);
-        } else if (typeOfTransportPackage == SELECT_GAME_TYPE) {
-            onSelectGameType(webSocket, transportPackageOfClient);
-        } else if (typeOfTransportPackage == ADD_VIEW) {
-            onAddView(webSocket, transportPackageOfClient);
-        } else if (typeOfTransportPackage == CREATE_NEW_GAME) {
-            onCreateNewGame(webSocket, transportPackageOfClient);
-        } else {
-            System.err.println("Server doesn't know received typeOfTransportPackage.");
-            System.err.println("typeOfTransportPackage = " + typeOfTransportPackage);
-            System.exit(1);
-        }
-        System.out.println("----------");
+        new IncomingMessageHandler(this, webSocket, byteBuffer);
     }
 
     @Override
@@ -174,113 +125,5 @@ public class MultiGameWebSocketServer extends WebSocketServer implements Transpo
         System.err.println(webSocket);
         System.err.println("This type of message (String) should not be!");
         System.exit(1);
-    }
-
-    protected void onLogout(WebSocket webSocket) {
-        System.out.println("onLogout");
-
-        sendLogoutAnswer(webSocket);
-    }
-
-    protected void onLogin(WebSocket webSocket, TransportPackageOfClient transportPackageOfClient) {
-        System.out.println("onLogin");
-
-        String userName = ((String) transportPackageOfClient.get("userName"));
-        String password = ((String) transportPackageOfClient.get("password"));
-
-        System.out.println("userName = " + userName + " | " + "password = *"); // Пароль не выводим:
-
-        // Имя пользователя и пароль извлечены. Теперь можно в отдельном потоке (вероятно в том, который был
-        // создан при onOpen) сверить входящие имя пользователя и пароль с теми, которые есть на сервере.
-        // И результат отправить клиенту.
-        // Но сейчас (пока с отдельными нитями не начали реализовывать) запросим в текущей нити.
-
-        if (Credentials.isUserAndPasswordCorrect(userName, password)) {
-            // ToDo: Нужно зафиксировать для этого webSocket имя пользователя (и потом другие параметры авторизации).
-            sendLoginAnswer(webSocket, userName);
-        } else {
-            sendLogoutAnswer(webSocket);
-        }
-    }
-
-    private void sendLogoutAnswer(WebSocket webSocket) {
-        send(webSocket, new TransportPackageOfServer(LOGOUT));
-    }
-
-    private void sendLoginAnswer(WebSocket webSocket, String userName) {
-        send(webSocket, new TransportPackageOfServer(
-                LOGIN,
-                Map.of("userName", userName)
-        ));
-    }
-
-    protected void onForgetGameTypeSet(WebSocket webSocket) {
-        System.out.println("onForgetGameTypeSet");
-        send(webSocket, new TransportPackageOfServer(FORGET_GAME_TYPE_SET));
-    }
-
-    protected void onGetGameTypeSet(WebSocket webSocket) {
-        System.out.println("onGetGameTypeSet");
-        send(webSocket, new TransportPackageOfServer(
-                GET_GAME_TYPE_SET,
-                Map.of("gameTypeSet",
-                        Stream.of(
-                                // ToDo: Перечень классов вариантов игр следует делать не константами в коде. Варианты:
-                                //       - файл параметров,
-                                //       - классы, хранящиеся по определённому пути.
-                                /*MinesweeperModel.class,*/
-                                SokobanModel.class
-                        ).collect(toList())
-                )
-        ));
-    }
-
-    protected void onForgetGameType(WebSocket webSocket) {
-        System.out.println("onForgetGameType");
-
-        System.out.println("modelOfServer = " + modelOfServer);
-        modelOfServer = null;
-        System.out.println("modelOfServer = " + modelOfServer);
-        send(webSocket, new TransportPackageOfServer(FORGET_GAME_TYPE));
-    }
-
-    protected void onSelectGameType(WebSocket webSocket, TransportPackageOfClient transportPackageOfClient) {
-        System.out.println("onSelectGameType");
-
-        // System.out.println("transportPackageOfClient = " + transportPackageOfClient);
-        String model = (String) transportPackageOfClient.get("gameType");
-        // ToDo: Проверить, что model одна из списка возможных моделей, которые были отправлены ранее этому клиенту.
-        //       И если это не так, то отправить клиенту FORGET_GAME_TYPE.
-
-        {   // Создать новую модель
-            System.out.println("modelOfServer = " + modelOfServer);
-            // ToDo: Вместо вызова конкретного конструктора, тут нужно создавать экземпляр того типа, который был выбран клиентом.
-            modelOfServer = new ModelOfServerOfSokoban<>(this);
-            System.out.println("modelOfServer = " + modelOfServer);
-        }
-
-        send(webSocket, new TransportPackageOfServer(
-                SELECT_GAME_TYPE,
-                Map.of("gameType", model)
-        ));
-    }
-
-    protected void onAddView(WebSocket webSocket, TransportPackageOfClient transportPackageOfClient) {
-        System.out.println("onAddView");
-
-        String viewId = (String) transportPackageOfClient.get("viewId");
-        // System.out.println("viewId = " + viewId);
-        modelOfServer.addRemoteView(new RemoteView<>(webSocket, viewId));
-
-        send(webSocket, new TransportPackageOfServer(
-                ADD_VIEW,
-                Map.of("viewId", viewId)
-        ));
-    }
-
-    protected void onCreateNewGame(WebSocket webSocket, TransportPackageOfClient transportPackageOfClient) {
-        System.out.println("onCreateNewGame");
-
-        modelOfServer.createNewGame();
     }
 }
