318bd4937a596cc24b7e587994b65203163d9e8e
“Maksim <m.n.timchenko@gmail.com>
2024-01-11 16:43:49 +0300

    refactor - move class ModelOfServerLoader (with models.txt file ) from module WebSocketServer to BaseTileModel

--- WebSocketServer/src/main/java/module-info.java
+++ WebSocketServer/src/main/java/module-info.java
@@ -3,7 +3,4 @@ module WebSocketServer {
     requires org.apache.commons.collections4;
 
     requires timmax.tilegame.basetilemodel;
-
-    requires timmax.tilegame.game.sokoban.model;
-    requires timmax.tilegame.game.minesweeper.model;
 }
