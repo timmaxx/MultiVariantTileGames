265a152a074c9a36ca8164245dd1b4e24a1f9c4d
“Maksim <m.n.timchenko@gmail.com>
2024-01-15 22:08:50 +0300

    prepare to refactor classes AbstractClientState and RemoteClientState

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/AbstractClientState.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/AbstractClientState.java
@@ -120,3 +120,132 @@ public abstract class AbstractClientState<Model> {
         // throw new RuntimeException("Unknown state.");
     }
 }
+
+/*
+package timmax.tilegame.basemodel.protocol;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import timmax.tilegame.basemodel.clientappstatus.MainGameClientStatus;
+import timmax.tilegame.basemodel.protocol.server.ModelOfServerDescriptor;
+
+public abstract class AbstractClientState<Model> {
+    // Нумерация приведена соответствующая классам Pane0Х... пакета timmax.tilegame.client.statuscontrol:
+    protected String userName = ""; // ---- 2 (Пользователь)
+    protected Set<ModelOfServerDescriptor> setOfModelOfServerDescriptor = new HashSet<>(); // ---- 3 (Список типов игр)
+    protected ModelOfServerDescriptor modelOfServerDescriptor; // ---- 4 (Конкретный тип игры)
+    protected List<Model> listOfServerBaseModel = new ArrayList<>(); // ---- 5 (Список моделей игр)
+    protected Model serverBaseModel = null; // ---- 6 (Конкретная модель игры)
+
+    // ---- 2 (Пользователь)
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        if (userName == null || userName.isEmpty()) {
+            throw new NullPointerException("UserName is null. It must be not null for this method.");
+        }
+        setUserName_(userName);
+    }
+
+    public void forgetUserName() {
+        setUserName_("");
+    }
+
+    private void setUserName_(String userName) {
+        setGameTypeSet_(null);
+        this.userName = userName;
+    }
+
+    // ---- 3 (Список типов игр)
+    public Set<ModelOfServerDescriptor> getGameTypeSet() {
+        return setOfModelOfServerDescriptor;
+    }
+
+    public void setGameTypeSet(Set<ModelOfServerDescriptor> setOfModelOfServerDescriptor) {
+        // ToDo: сделать какую-то проверку на корректностность.
+        setGameTypeSet_(setOfModelOfServerDescriptor);
+    }
+
+    public void forgetGameTypeSet() {
+        setGameTypeSet_(null);
+    }
+
+    private void setGameTypeSet_(Set<ModelOfServerDescriptor> setOfModelOfServerDescriptor) {
+        this.setOfModelOfServerDescriptor = setOfModelOfServerDescriptor;
+    }
+
+    // ---- 4 (Конкретный тип игры)
+    // Наверное не Model должен быть типом. Вероятнее это ModelOfServerDescriptor
+    public Model getGameType() {
+        return serverBaseModel;
+    }
+
+    public void setGameType(Model modelOfServer) {
+        setGameType_(modelOfServer);
+    }
+
+    public void forgetGameType() {
+        setGameType_(null);
+    }
+
+    private void setGameType_(Model modelOfServer) {
+        this.serverBaseModel = modelOfServer;
+    }
+
+    // ---- 5 (Список моделей игр)
+    public List<Model> getListOfServerBaseModel() {
+        return listOfServerBaseModel;
+    }
+
+    public void setListOfServerBaseModel(List<Model> listOfServerBaseModel) {
+        setListOfServerBaseModel_(listOfServerBaseModel);
+    }
+
+    public void forgetListOfServerBaseModel() {
+        setListOfServerBaseModel_(new ArrayList<>());
+    }
+
+    private void setListOfServerBaseModel_(List<Model> listOfServerBaseModel) {
+        forgetServerBaseModel();
+        this.listOfServerBaseModel = listOfServerBaseModel;
+    }
+
+    // ---- 6 (Конкретная модель игры)
+    public Model getServerBaseModel() {
+        return serverBaseModel;
+    }
+
+    public void setServerBaseModel(Model serverBaseModel) {
+        setServerBaseModel_(serverBaseModel);
+    }
+
+    public void forgetServerBaseModel() {
+        setServerBaseModel_(null);
+    }
+
+    private void setServerBaseModel_(Model serverBaseModel) {
+        this.serverBaseModel = serverBaseModel;
+    }
+
+    // ---- X
+    public MainGameClientStatus getMainGameClientStatus() {
+        if (userName.equals("")) {
+            return MainGameClientStatus.CONNECT_NON_IDENT;
+        } else {
+            if (listOfServerBaseModel.size() > 0) {
+                if (serverBaseModel != null) {
+                    return MainGameClientStatus.GAME_TYPE_SELECT;
+                }
+                return MainGameClientStatus.GET_GAME_TYPE_SET;
+            }
+            return MainGameClientStatus.CONNECT_AUTHORIZED;
+        }
+        // throw new RuntimeException("Unknown state.");
+    }
+}
+*/
--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server/RemoteClientState.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server/RemoteClientState.java
@@ -60,3 +60,83 @@ public class RemoteClientState<ClienId> extends AbstractClientState<IModelOfServ
         transportOfServer.sendEventOfServer(clientId, new EventOfServer31GameTypeSelect(modelOfServer.getGameName()));
     }
 }
+
+/*
+package timmax.tilegame.basemodel.protocol.server;
+
+import java.util.List;
+import java.util.Set;
+
+import timmax.tilegame.basemodel.protocol.*;
+import timmax.tilegame.transport.TransportOfServer;
+
+public class RemoteClientState<ClienId> extends AbstractClientState<IModelOfServer<ClienId>> {
+    private final ClienId clientId;
+    private final TransportOfServer<ClienId> transportOfServer;
+
+    public RemoteClientState(TransportOfServer<ClienId> transportOfServer, ClienId clientId) {
+        this.clientId = clientId;
+        this.transportOfServer = transportOfServer;
+    }
+
+    // ---- 2 (Пользователь)
+    @Override
+    public void forgetUserName() {
+        super.forgetUserName();
+        transportOfServer.sendEventOfServer(clientId, new EventOfServer10Logout());
+    }
+
+    @Override
+    public void setUserName(String userName) {
+        super.setUserName(userName);
+        transportOfServer.sendEventOfServer(clientId, new EventOfServer11Login(userName));
+    }
+
+    // ---- 3 (Список типов игр)
+    public Set<ModelOfServerDescriptor> getGameTypeSet() {
+        return setOfModelOfServerDescriptor;
+    }
+
+    @Override
+    public void forgetGameTypeSet() {
+        super.forgetGameTypeSet();
+        transportOfServer.sendEventOfServer(clientId, new EventOfServer20ForgetGameTypeSet());
+    }
+
+    @Override
+    public void setGameTypeSet(Set<ModelOfServerDescriptor> setOfModelOfServerDescriptor) { // ToDo: get или set (как 'setUserName(String userName)')???
+        super.setGameTypeSet(setOfModelOfServerDescriptor);
+        // ToDo: Если set, то следующее значение передавать сюда параметром,
+        //       вычисление которого как раз и вынести в вызывающую логику.
+        List<String> listOfServerBaseModelString = transportOfServer
+                .getCollectionOfModelOfServerDescriptor()
+                .stream()
+                .map(ModelOfServerDescriptor::getGameName)
+                .toList();
+        transportOfServer.sendEventOfServer( clientId, new EventOfServer21GetGameTypeSet(listOfServerBaseModelString));
+*/
+/*
+        Set<ModelOfServerDescriptor> setOfModelOfServerDescriptor = transportOfServer
+                .getCollectionOfModelOfServerDescriptor()
+                .stream()
+                .collect(Collectors.toSet());
+                ;
+        // transportOfServer.sendEventOfServer( clientId, new EventOfServer21GetGameTypeSet(setOfModelOfServerDescriptor));
+        *//*
+
+    }
+
+    // ---- 4 (Конкретный тип игры)
+    @Override
+    public void forgetGameType() {
+        super.forgetGameType();
+        transportOfServer.sendEventOfServer(clientId, new EventOfServer30ForgetGameType());
+    }
+
+    @Override
+    public void setGameType(IModelOfServer<ClienId> modelOfServer) {
+        super.setGameType(modelOfServer);
+        transportOfServer.sendEventOfServer(clientId, new EventOfServer31GameTypeSelect(modelOfServer.getGameName()));
+    }
+}
+*/
