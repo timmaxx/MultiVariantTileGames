1266f2cb78eb31366c912deb68a0eb0c867a9aea
“Maksim <m.n.timchenko@gmail.com>
2024-02-13 23:01:59 +0300

    add classOfView into class EventOfServer91AddView

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/EventOfServer91AddView.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/EventOfServer91AddView.java
@@ -4,43 +4,46 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 
+import timmax.tilegame.baseview.View;
 import timmax.tilegame.transport.TransportOfClient;
 
 public class EventOfServer91AddView extends EventOfServer {
-    private String viewId;
+    private Class<? extends View> classOfView;
+    private String viewName;
 
     public EventOfServer91AddView() {
         super();
     }
 
-    public EventOfServer91AddView(String viewId) {
+    public EventOfServer91AddView(Class<? extends View> classOfView, String viewName) {
         this();
-        this.viewId = viewId;
+        this.classOfView = classOfView;
+        this.viewName = viewName;
     }
 
     @Override
     public void executeOnClient(TransportOfClient transportOfClient) {
         System.out.println("  onAddView");
-
-        System.out.println("    viewId = " + viewId);
-
-        transportOfClient.getLocalClientState().confirmView(viewId);
+        transportOfClient.getLocalClientState().addView(classOfView, viewName);
     }
 
     @Override
     public String toString() {
-        return "EventOfServer41AddView{" +
-                "viewId='" + viewId + '\'' +
+        return "EventOfServer91AddView{" +
+                "classOfView=" + classOfView +
+                ", viewName='" + viewName + '\'' +
                 '}';
     }
 
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeObject(viewId);
+        out.writeObject(classOfView);
+        out.writeObject(viewName);
     }
 
     @Override
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        viewId = (String) in.readObject();
+        classOfView = (Class<? extends View>) in.readObject();
+        viewName = (String) in.readObject();
     }
 }
