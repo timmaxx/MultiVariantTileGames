33e5d75de25df4664031737a1051760135f3ae18
“Maksim <m.n.timchenko@gmail.com>
2024-02-15 08:57:00 +0300

    fix ListOfLocalView::getViewByViewId()

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/client/ListOfLocalView.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/client/ListOfLocalView.java
@@ -7,7 +7,7 @@ import timmax.tilegame.baseview.View;
 public class ListOfLocalView extends ArrayList<View> {
     public View getViewByViewId(String viewId) {
         for (View view : this) {
-            if (view.toString().equals(viewId)) {
+            if (view.getViewName().equals(viewId)) {
                 return view;
             }
         }
