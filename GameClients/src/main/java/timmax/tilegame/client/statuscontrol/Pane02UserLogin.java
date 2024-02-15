b9f5e8a53d7c2809ba929469ef8251b9f58f6ca4
“Maksim <m.n.timchenko@gmail.com>
2024-02-15 15:29:00 +0300

    prepare class Pane02UserLogin for unification of methods doOnPrevPrevState(), doOnPrevState(), doOnNextState()

--- GameClients/src/main/java/timmax/tilegame/client/statuscontrol/Pane02UserLogin.java
+++ GameClients/src/main/java/timmax/tilegame/client/statuscontrol/Pane02UserLogin.java
@@ -67,7 +67,6 @@ public class Pane02UserLogin extends AbstractConnectStatePane {
         doOnNextState();
     }
 
-
     //
     protected void doOnPrevPrevState() {
         disableAllControls();
