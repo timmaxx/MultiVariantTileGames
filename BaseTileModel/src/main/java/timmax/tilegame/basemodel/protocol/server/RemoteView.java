18efdcc2cb60ecb95448df564c4d6efcdfcf8a13
“Maksim <m.n.timchenko@gmail.com>
2024-02-18 11:11:54 +0300

    simplest refactor class RemoteView

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server/RemoteView.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server/RemoteView.java
@@ -2,12 +2,11 @@ package timmax.tilegame.basemodel.protocol.server;
 
 import java.util.Objects;
 
-// В экземпляре этого класса будет сохранятся идентификатор клиента и строковый идентификатор выборки,
+// В экземпляре этого класса будет храниться идентификатор клиента и строковый идентификатор выборки,
 // которой нужно отправить то или иное сообщение об игровом событии.
 public class RemoteView<ClientId> {
     private final ClientId clientId;
     private final String viewName;
-
 
     public RemoteView(ClientId clientId, String viewName) {
         this.clientId = clientId;
