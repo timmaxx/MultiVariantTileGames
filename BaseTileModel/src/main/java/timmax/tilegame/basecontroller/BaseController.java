23367b9f31fec61719ad8ba0be6aeac0a2eb34e2
“Maksim <m.n.timchenko@gmail.com>
2024-02-27 23:00:44 +0300

    change calling System.out.println() to using slf4j logging in class BaseController

--- BaseTileModel/src/main/java/timmax/tilegame/basecontroller/BaseController.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basecontroller/BaseController.java
@@ -2,6 +2,8 @@ package timmax.tilegame.basecontroller;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import timmax.tilegame.basemodel.gamecommand.GameCommand;
 import timmax.tilegame.basemodel.gamecommand.GameCommandKeyPressed;
@@ -11,6 +13,8 @@ import timmax.tilegame.basemodel.protocol.EventOfClient92GameCommand;
 import timmax.tilegame.transport.TransportOfClient;
 
 public class BaseController {
+    protected static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
     private final TransportOfClient transportOfClient;
 
     public BaseController(TransportOfClient transportOfClient) {
@@ -18,16 +22,16 @@ public class BaseController {
     }
 
     public void onKeyPressed(KeyCode keyCode) {
-        System.out.println("BaseController::onKeyPressed()");
-        System.out.println("  keyCode = " + keyCode);
+        logger.info("onKeyPressed()");
+        logger.info("  keyCode = {}", keyCode);
         GameCommand gameCommand = new GameCommandKeyPressed(keyCode);
         EventOfClient eventOfClient = new EventOfClient92GameCommand(gameCommand);
         sendEventOfClient(eventOfClient);
     }
 
     public final void onMouseClick(MouseButton mouseButton, int x, int y) {
-        System.out.println("BaseController::onMouseClick()");
-        System.out.println("  mouseButton = " + mouseButton + ", x = " + x + ", y = " + y);
+        logger.info("onMouseClick()");
+        logger.info("  mouseButton = {}, x = {}, y = {}", mouseButton, x, y);
         GameCommand gameCommand = new GameCommandMouseClick(x, y, mouseButton);
         EventOfClient eventOfClient = new EventOfClient92GameCommand(gameCommand);
         sendEventOfClient(eventOfClient);
