e36f3fe419293e9208ad58a97445785832ca6b45
“Maksim <m.n.timchenko@gmail.com>
2024-02-28 19:15:44 +0300

    delete logging from BaseController class

--- BaseTileModel/src/main/java/timmax/tilegame/basecontroller/BaseController.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basecontroller/BaseController.java
@@ -2,8 +2,6 @@ package timmax.tilegame.basecontroller;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import timmax.tilegame.basemodel.gamecommand.GameCommand;
 import timmax.tilegame.basemodel.gamecommand.GameCommandKeyPressed;
@@ -13,8 +11,6 @@ import timmax.tilegame.basemodel.protocol.EventOfClient92GameCommand;
 import timmax.tilegame.transport.TransportOfClient;
 
 public class BaseController {
-    protected static final Logger logger = LoggerFactory.getLogger(BaseController.class);
-
     private final TransportOfClient transportOfClient;
 
     public BaseController(TransportOfClient transportOfClient) {
@@ -22,16 +18,12 @@ public class BaseController {
     }
 
     public void onKeyPressed(KeyCode keyCode) {
-        logger.info("onKeyPressed()");
-        logger.info("  keyCode = {}", keyCode);
         GameCommand gameCommand = new GameCommandKeyPressed(keyCode);
         EventOfClient eventOfClient = new EventOfClient92GameCommand(gameCommand);
         sendEventOfClient(eventOfClient);
     }
 
     public final void onMouseClick(MouseButton mouseButton, int x, int y) {
-        logger.info("onMouseClick()");
-        logger.info("  mouseButton = {}, x = {}, y = {}", mouseButton, x, y);
         GameCommand gameCommand = new GameCommandMouseClick(x, y, mouseButton);
         EventOfClient eventOfClient = new EventOfClient92GameCommand(gameCommand);
         sendEventOfClient(eventOfClient);
