24378b3ae6c9c09e5c4de4ed93cf5f9db0f62a89
“Maksim <m.n.timchenko@gmail.com>
2024-06-25 19:49:19 +0300

    delete unused code (in comments) from LocalClientStateAutomaton class

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/client/LocalClientStateAutomaton.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/client/LocalClientStateAutomaton.java
@@ -6,32 +6,23 @@ import timmax.tilegame.basemodel.protocol.server_client.ClientStateAutomaton;
 import timmax.tilegame.basemodel.protocol.server_client.IFabricOfClientStateAutomaton;
 import timmax.tilegame.basemodel.protocol.server_client.IFabricOfClientStates;
 import timmax.tilegame.baseview.View;
-// import timmax.tilegame.transport.TransportOfClient;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LocalClientStateAutomaton<Model> extends ClientStateAutomaton<Model> {
-    // ToDo: По аналогии с RemoteClientStateAutomaton включу сюда TransportOfClient.
-    // private final TransportOfClient transportOfClient;
     private final HashSetOfObserverOnAbstractEvent hashSetOfObserverOnAbstractEvent;
     private final Map<String, View> mapOfViewName_View;
 
     public LocalClientStateAutomaton(
             IFabricOfClientStates<Model> IFabricOfClientStates,
-            IFabricOfClientStateAutomaton iFabricOfClientStateAutomaton/*,
-            TransportOfClient transportOfClient*/) {
+            IFabricOfClientStateAutomaton iFabricOfClientStateAutomaton) {
         super(IFabricOfClientStates, iFabricOfClientStateAutomaton);
 
-        // this.transportOfClient = transportOfClient;
         hashSetOfObserverOnAbstractEvent = new HashSetOfObserverOnAbstractEvent();
         mapOfViewName_View = new HashMap<>();
     }
-/*
-    public TransportOfClient getTransportOfClient() {
-        return transportOfClient;
-    }
-*/
+
     public HashSetOfObserverOnAbstractEvent getHashSetOfObserverOnAbstractEvent() {
         return hashSetOfObserverOnAbstractEvent;
     }
