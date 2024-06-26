75b307f9164043a4e5febfc7ee1a84d333c8c26a
“Maksim <m.n.timchenko@gmail.com>
2024-06-26 15:10:21 +0300

    delete parameter <Model> from classes LocalClientStateAutomaton, LocalClientState0X..., EventOfServer, EventOfServerXX..., FabricOfClientStatesJfx, MultiGameWebSocketClient, MultiGameWebSocketClientManyTimesUse

--- TileGameEngineJFX/src/main/java/timmax/tilegame/basemodel/protocol/client/jfx/FabricOfClientStatesJfx.java
+++ TileGameEngineJFX/src/main/java/timmax/tilegame/basemodel/protocol/client/jfx/FabricOfClientStatesJfx.java
@@ -5,42 +5,42 @@ import timmax.tilegame.basemodel.protocol.server_client.*;
 
 public class FabricOfClientStatesJfx implements IFabricOfClientStates<InstanceIdOfModel> {
     @Override
-    public ClientState01NoConnect<InstanceIdOfModel> getClientState01NoConnect(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState01NoConnect getClientState01NoConnect(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState01NoConnect(clientStateAutomaton);
     }
 
     @Override
-    public ClientState02ConnectNonIdent<InstanceIdOfModel> getClientState02ConnectNonIdent(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState02ConnectNonIdent getClientState02ConnectNonIdent(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState02ConnectNonIdent(clientStateAutomaton);
     }
 
     @Override
-    public ClientState03ConnectAuthorized<InstanceIdOfModel> getClientState03ConnectAuthorized(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState03ConnectAuthorized getClientState03ConnectAuthorized(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState03ConnectAuthorized(clientStateAutomaton);
     }
 
     @Override
-    public ClientState04GameTypeSetSelected<InstanceIdOfModel> getClientState04GameTypeSetSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState04GameTypeSetSelected getClientState04GameTypeSetSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState04GameTypeSetSelected(clientStateAutomaton);
     }
 
     @Override
-    public ClientState05GameTypeSelected<InstanceIdOfModel> getClientState05GameTypeSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState05GameTypeSelected getClientState05GameTypeSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState05GameTypeSelected(clientStateAutomaton);
     }
 
     @Override
-    public ClientState06GameMatchSetSelected<InstanceIdOfModel> getClientState06GameMatchSetSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState06GameMatchSetSelected getClientState06GameMatchSetSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState06GameMatchSetSelected(clientStateAutomaton);
     }
 
     @Override
-    public ClientState07GameMatchSelected<InstanceIdOfModel> getClientState07GameMatchSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState07GameMatchSelected getClientState07GameMatchSelected(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState07GameMatchSelected(clientStateAutomaton);
     }
 
     @Override
-    public ClientState08GameIsPlaying<InstanceIdOfModel> getClientState08GameIsPlaying(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
+    public LocalClientState08GameIsPlaying getClientState08GameIsPlaying(ClientStateAutomaton<InstanceIdOfModel> clientStateAutomaton) {
         return new LocalClientState08GameIsPlaying(clientStateAutomaton);
     }
 }
