b60ad29181e03c063a2f5dff56195664634c0acc
“Maksim <m.n.timchenko@gmail.com>
2024-06-29 23:39:56 +0300

    delete parameter <Model> from classes LocalClientStateAutomaton, LocalClientState0X..., EventOfServer, EventOfServerXX..., FabricOfClientStatesJfx, MultiGameWebSocketClient, MultiGameWebSocketClientManyTimesUse

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/EventOfServer.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/EventOfServer.java
@@ -2,7 +2,6 @@ package timmax.tilegame.basemodel.protocol;
 
 import timmax.tilegame.basemodel.protocol.client.LocalClientStateAutomaton;
 
-// ToDo: Устранить параметризацию от Model
 public abstract class EventOfServer extends Event {
     public abstract void executeOnClient(LocalClientStateAutomaton localClientStateAutomaton);
 
