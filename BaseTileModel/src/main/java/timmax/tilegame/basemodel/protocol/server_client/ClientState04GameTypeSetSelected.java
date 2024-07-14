2866907fa6ef1ce7673cc495f6127fdcb17bd6c6
timmaxx <m.n.timchenko@gmail.com>
2024-07-14 12:27:58 +1000

    delete intermediate state 03ConnectAuthorized (classes: ClientState03 (+Local +Remote), EventOf...20 (Server and Client), EventOf...31 (Server and Client), Pane03GetGameTypeSet)

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server_client/ClientState04GameTypeSetSelected.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server_client/ClientState04GameTypeSetSelected.java
@@ -2,6 +2,7 @@ package timmax.tilegame.basemodel.protocol.server_client;
 
 import timmax.tilegame.basemodel.protocol.server.GameType;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public abstract class ClientState04GameTypeSetSelected<Model> extends AbstractClientState<Model> implements IClientState04GameTypeSetSelected {
@@ -17,7 +18,7 @@ public abstract class ClientState04GameTypeSetSelected<Model> extends AbstractCl
 
     @Override
     public void forgetGameTypeSet() {
-        getClientStateAutomaton().setGameTypeSet_(null);
+        getClientStateAutomaton().setGameTypeSet_(new HashSet<>());
     }
 
     @Override
