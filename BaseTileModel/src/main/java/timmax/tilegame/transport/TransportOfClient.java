be5e7418af279533e44e60c43a4c2bbe39988d71
timmaxx <m.n.timchenko@gmail.com>
2024-07-19 00:18:03 +1000

    edit comments in interface TransportOfClient

--- BaseTileModel/src/main/java/timmax/tilegame/transport/TransportOfClient.java
+++ BaseTileModel/src/main/java/timmax/tilegame/transport/TransportOfClient.java
@@ -23,24 +23,24 @@ public interface TransportOfClient {
 
     LocalClientStateAutomaton getLocalClientStateAutomaton();
 
-    // Методы ниже я перевёл сюда из интерфейса IModelOfClient (уже удалён).
     // Методы этого интерфейса возвращают void т.к. все они исполняются на стороне клиента и не ждут синхронного ответа от сервера.
     // В правой колонке, в комментариях, даны соответствующие методы интерфейсов IClientState0Х...
+    // ToDo: Поскольку есть совпадение имён методов и х назначений, возможно свести их в единую систему.
     // ---- 2 ConnectNonIdent
-    void setUser(String userName, String password);                             //  22  //  2 -> 3  void setUser(String userName);
+    void setUser(String userName, String password);                             //  2 -> 4  void setUser(String userName, Set<GameType> gameTypeSet);
 
     // ---- 4 (GameTypeSetSelected)
-    void forgetUser();                                                          //  40  //  4 -> 2  void forgetUser();
-    void setGameType(GameType gameType);                                        //  41  //  4 -> 5  void setGameType(GameType gameType);
+    void forgetUser();                                                          //  4 -> 2  void forgetUser();
+    void setGameType(GameType gameType);                                        //  4 -> 5  void setGameType(GameType gameType, Set<GameMatchX> gameMatchXSet);
 
     // ---- 6 (MatchSetSelected)
-    void forgetGameType();                                                      //  60  //  6 -> 5  void forgetGameMatchSet();
-    void setGameMatch(GameMatchId gameMatchId);                                 //  61  //  6 -> 7  void setGameMatch(Model serverBaseModel);
+    void forgetGameType();                                                      //  6 -> 5  void forgetGameType();
+    void setGameMatch(GameMatchId gameMatchId);                                 //  6 -> 7  void setGameMatchX(GameMatchX gameMatchX);
 
     // ---- 7 (MatchSelected)
-    void forgetGameMatch();                                                     //  70  //  7 -> 6  void forgetGameMatch();
-    void setGameMatchPlaying(Map<String, Integer> mapOfParamsOfModelValue);     //  71  //  7 -> 8  void setGameMatchPlaying(Boolean gameIsPlaying);
+    void forgetGameMatch();                                                     //  7 -> 6  void forgetGameMatchX();
+    void setGameMatchPlaying(Map<String, Integer> mapOfParamsOfModelValue);     //  7 -> 8  void setGameMatchPlaying(Boolean gameIsPlaying);
 
     // ---- 8 (GameIsPlaying)
-    void forgetGameMatchPlaying();                                              //  80  //  8 -> 7  void forgetGameMatchPlaying();
+    void forgetGameMatchPlaying();                                              //  8 -> 7  void forgetGameMatchPlaying();
 }
