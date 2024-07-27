095325e49cd2aa7e109bd3f6c6e5eff05d88eaa8
timmaxx <m.n.timchenko@gmail.com>
2024-07-27 14:16:14 +1000

    delete some unlikely classes

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server/IControlSet.java
+++
@@ -1,5 +0,0 @@
-package timmax.tilegame.basemodel.protocol.server;
-
-// См. комменты к Pane07GameMatchSelected :: void updateOnSetGameMatch()
-public interface IControlSet {
-}
--- GameClients/src/main/java/timmax/tilegame/client/statuscontrol/ControlSetJfx.java
+++
@@ -1,9 +0,0 @@
-package timmax.tilegame.client.statuscontrol;
-
-import timmax.tilegame.basemodel.protocol.server.IControlSet;
-
-import java.util.HashSet;
-
-// См. комменты к Pane07GameMatchSelected :: void updateOnSetGameMatch()
-public class ControlSetJfx extends HashSet<ControlJfx> implements IControlSet {
-}
