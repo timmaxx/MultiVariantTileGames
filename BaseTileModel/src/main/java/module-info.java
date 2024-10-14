15df873508640d602fc8cc427c6c299f98412ed1
timmaxx <m.n.timchenko@gmail.com>
2024-10-14 16:17:33 +0300

    fix some errors (BaseTileModel)

--- BaseTileModel/src/main/java/module-info.java
+++ BaseTileModel/src/main/java/module-info.java
@@ -6,7 +6,6 @@ module timmax.tilegame.basetilemodel {
     exports timmax.tilegame.basemodel;
     exports timmax.tilegame.baseview;
     exports timmax.tilegame.basecontroller;
-    exports timmax.tilegame.basemodel.tile;
     exports timmax.tilegame.basemodel.gameevent;
     exports timmax.tilegame.basemodel.gamecommand;
     exports timmax.tilegame.basemodel.protocol;
