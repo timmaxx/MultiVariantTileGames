7fc381856dd57cb2c90c243a87d7fcc1b780af81
timmaxx <m.n.timchenko@gmail.com>
2024-12-05 22:42:41 +0300

    move Set<GameType> gameTypeSet from EventOfServer21IdentifyAuthenticateAuthorizeUser to EventOfServer11ConnectWithoutUserIdentify

--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server_client/IClientState02ConnectNonIdent.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server_client/IClientState02ConnectNonIdent.java
@@ -1,10 +1,17 @@
 package timmax.tilegame.basemodel.protocol.server_client;
 
+import timmax.tilegame.basemodel.protocol.server.GameType;
+
+import java.util.Set;
+
 //  Подключение есть, но сервер не идентифицировал, не аутентифицировал и не авторизовал пользователя.
 public interface IClientState02ConnectNonIdent extends IClientState00 {
     // Нумерация приведена соответствующая классам Pane0Х... пакета timmax.tilegame.client.statuscontrol:
 
     // ---- 2 ConnectNonIdent
+    //  Warning:(13, 9) Raw use of parameterized class 'GameType'
+    Set<GameType> getGameTypeSet();
+
     void authorizeUser(String userName);
     //  interface TransportOfClient
     //      void identifyAuthenticateAuthorizeUser(String userName, String password);
--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server_client/IClientState04GameTypeSetSelected.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/protocol/server_client/IClientState04GameTypeSetSelected.java
@@ -2,17 +2,11 @@ package timmax.tilegame.basemodel.protocol.server_client;
 
 import timmax.tilegame.basemodel.protocol.server.GameType;
 
-import java.util.Set;
-
 //  Выбран перечень типов игр.
 public interface IClientState04GameTypeSetSelected<GameMatchX extends IGameMatchX> extends IClientState00 {
     // Нумерация приведена соответствующая классам Pane0Х... пакета timmax.tilegame.client.statuscontrol:
 
     // ---- 4 (GameTypeSetSelected)
-    //  Warning:(12, 9) Raw use of parameterized class 'GameType'
-    Set<GameType> getGameTypeSet();
-
-
     void selectGameType(GameType<GameMatchX> gameType);
     //  interface TransportOfClient
     //      void selectGameType(GameType gameType);
