990ede623e5a8cd02ab0f3146fb9b753d89130a9
timmaxx <m.n.timchenko@gmail.com>
2025-01-06 13:03:36 +1000

    separate User to BaseEntity and User

---
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/credential/BaseEntity.java
@@ -0,0 +1,35 @@
+package timmax.tilegame.basemodel.credential;
+
+public class BaseEntity {
+    private final String id;
+
+    public BaseEntity(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
--- BaseTileModel/src/main/java/timmax/tilegame/basemodel/credential/User.java
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/credential/User.java
@@ -1,44 +1,21 @@
 package timmax.tilegame.basemodel.credential;
 
 //  Пользователь
-public class User {
-    private final String id;
+public class User extends BaseEntity {
     private final String password;
 
     //  ToDo:   Сделать поле перечень матчей, в которых этот пользователь является участником и под каким номером.
 
     public User(String id, String password) {
-        this.id = id;
+        super(id);
         this.password = password;
-    }
-
-    public String getId() {
-        return id;
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "id='" + id + '\'' +
+                "id='" + getId() + '\'' +
                 ", password='" + "*" + '\'' +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (!id.equals(user.id)) return false;
-        return password.equals(user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + password.hashCode();
-        return result;
     }
 }
