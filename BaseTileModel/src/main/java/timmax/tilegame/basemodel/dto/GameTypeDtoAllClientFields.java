779ccb2e0d3336b6c295bc14a8b8c4882a11b46d
timmaxx <m.n.timchenko@gmail.com>
2025-01-10 03:12:58 +1000

    add empty classes GameTypeDtoAllClientFields and GameTypeDtoGameMatchDtoSet

---
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/dto/GameTypeDtoAllClientFields.java
@@ -0,0 +1,33 @@
+package timmax.tilegame.basemodel.dto;
+
+public class GameTypeDtoAllClientFields extends BaseDto {
+/*
+    private int countOfGamers;
+    private Set<GameMatchDto> gameMatchDtoSet;
+    private Map<String, Class<? extends View>> viewName_ViewClassMap;
+    private ParamName_paramModelDescriptionMap paramName_paramModelDescriptionMap;
+
+    //  ToDo:   Поле относится к визуализации. Его нужно абстрагировать и вынести отсюда.
+    private GuiDefaultConstants guiDefaultConstants;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        out.writeInt(countOfGamers);
+        out.writeObject(gameMatchDtoSet);
+        out.writeObject(viewName_ViewClassMap);
+        out.writeObject(paramName_paramModelDescriptionMap);
+        out.writeObject(guiDefaultConstants);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        countOfGamers = in.readInt();
+        gameMatchDtoSet = (Set<GameMatchDto>) in.readObject();
+        viewName_ViewClassMap = (Map<String, Class<? extends View>>) in.readObject();
+        paramName_paramModelDescriptionMap = (ParamName_paramModelDescriptionMap) in.readObject();
+        guiDefaultConstants = (GuiDefaultConstants) in.readObject();
+    }
+*/
+}
---
+++ BaseTileModel/src/main/java/timmax/tilegame/basemodel/dto/GameTypeDtoGameMatchDtoSet.java
@@ -0,0 +1,19 @@
+package timmax.tilegame.basemodel.dto;
+
+public class GameTypeDtoGameMatchDtoSet extends BaseDto {
+/*
+    private Set<GameMatchDto> gameMatchDtoSet;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        out.writeObject(gameMatchDtoSet);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        gameMatchDtoSet = (Set<GameMatchDto>) in.readObject();
+    }
+*/
+}
