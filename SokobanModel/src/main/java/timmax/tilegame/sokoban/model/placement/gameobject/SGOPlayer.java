f2994737386131b8d1a06d93833416e3b384f8d2
timmaxx <m.n.timchenko@gmail.com>
2025-01-13 22:02:05 +0300

    refactor SGOPlayer :: void move(SokobanXYOffset xyOffset)

--- SokobanModel/src/main/java/timmax/tilegame/sokoban/model/placement/gameobject/SGOPlayer.java
+++ SokobanModel/src/main/java/timmax/tilegame/sokoban/model/placement/gameobject/SGOPlayer.java
@@ -3,11 +3,7 @@ package timmax.tilegame.sokoban.model.placement.gameobject;
 import timmax.tilegame.basemodel.exception.GameOverException;
 import timmax.tilegame.basemodel.placement.primitives.XYCoordinate;
 import timmax.tilegame.sokoban.model.placement.primitives.SokobanXYOffset;
-import timmax.tilegame.basemodel.placement.gameobject.GameObject;
 import timmax.tilegame.basemodel.placement.placementstate.GameObjectsPlacementStateAutomaton;
-import timmax.tilegame.sokoban.model.gameevent.GameEventSokobanVariableParamsCountOfBoxesInHouses;
-
-import java.util.Set;
 
 public class SGOPlayer extends SGOCollisionMovableObject {
     public SGOPlayer(String id, GameObjectsPlacementStateAutomaton gameObjectsPlacementStateAutomaton, XYCoordinate xyCoordinate) {
@@ -18,30 +14,17 @@ public class SGOPlayer extends SGOCollisionMovableObject {
     public void move(SokobanXYOffset xyOffset) {
         //  Координаты после смещения (новые)
         XYCoordinate xyCoordinateNew = getXyCoordinate().getXYCoordinateByOffset(xyOffset, getGameObjectsPlacement().getWidthHeightSizes());
-        //  Ищем множество объектов в новых координатах (оно может быть или пустым или с одним элементом)
-        Set<GameObject> gameObjectSetInNewPlace = getGameObjectsPlacement().getGameObjectSetFilteredByXYCoordinate(xyCoordinateNew);
-        //  В этом множестве фильтруем только коробки
-        GameObject gameObjectInNewPlace = gameObjectSetInNewPlace
+        getGameObjectsPlacement()
+                //  Ищем множество объектов в новых координатах, по направлению движения игрока
+                //  (оно может быть или пустым или с одним элементом)
+                .getGameObjectSetFilteredByXYCoordinate(xyCoordinateNew)
                 .stream()
+                //  В этом множестве фильтруем только коробки
                 .filter(go -> go instanceof SGOBox)
                 .findAny()
-                .orElse(null);
-
-        int countBoxesOnHomes = 0;
-        int countBoxes = 0;
-        //  Если коробка была найдена
-        if (gameObjectInNewPlace instanceof SGOBox sgoBox) {
-            //  Двигаем коробку
-            sgoBox.move(xyOffset);
-
-            //  Посчитаем количество коробок, стоящих на домах
-            for (GameObject gameObject : getGameObjectsPlacement().getGameObjectSetFilteredByGameObjectClass(SGOBox.class)) {
-                countBoxes++;
-                if (gameObject instanceof SGOBox sgoBox1) {
-                    countBoxesOnHomes += sgoBox1.countOnHome();
-                }
-            }
-        }
+                //  Если коробка была найдена, то перемещаем её
+                .ifPresent(sgoBox -> ((SGOBox) sgoBox).move(xyOffset))
+        ;
         //  Переместим игрока
         super.move(xyOffset);
 
